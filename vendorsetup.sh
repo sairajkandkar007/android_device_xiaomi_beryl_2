dea46e356a8367e4c1ac14c46bb505aba22b9054
sairajkandkar007 <sairajkandkar0110@gmail.com>
2026-09-19 20:53:57 +0530

    Update keymaster version in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
+
 export FOX_BUILD_DEVICE="beryl"
+
+# A/B
 export FOX_AB_DEVICE=1
+
+# Virtual A/B
 export FOX_VIRTUAL_AB_DEVICE=1
+
+# Recovery is stored in vendor_boot
+export FOX_VENDOR_BOOT_RECOVERY=1
+
+# Allow missing Android dependencies
 export ALLOW_MISSING_DEPENDENCIES=true
+
+# Build environment
 export LC_ALL="C"
+
+# Use stock/prebuilt kernel
 export OF_FORCE_PREBUILT_KERNEL=1
-export OF_DEFAULT_KEYMASTER_VERSION=4.1
+
+# Keymaster
+export OF_DEFAULT_KEYMASTER_VERSION=4.0
