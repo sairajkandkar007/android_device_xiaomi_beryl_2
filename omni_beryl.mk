666e902f29670b276f23fd0ab61766444638ff67
sairajkandkar007 <sairajkandkar0110@gmail.com>
2026-09-19 19:20:51 +0530

    Clean up comments in omni_beryl.mk
    Removed copyright and SPDX license comments from omni_beryl.mk.

--- omni_beryl.mk
+++ omni_beryl.mk
@@ -1,18 +1,6 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from beryl device
 $(call inherit-product, device/xiaomi/beryl/device.mk)
 
 PRODUCT_DEVICE := beryl
